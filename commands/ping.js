885df00451c4d4cf275c6abd93c453ca25006cf7
JPlexer <32966779+JPlexer@users.noreply.github.com>
2018-11-25 19:09:19 +0100

    Add files via upload

--- commands/ping.js
+++ commands/ping.js
@@ -1,9 +1,9 @@
 exports.run = (message, Discord) => {
     embed = new Discord.RichEmbed();
     embed.setColor("#00FFFB");
-    embed.setAuthor(`${functions.branch} Ping`);
+    embed.setAuthor(`${config.branch} Ping`);
     embed.addField("Pong!", `It took me \`${`${Date.now() - message.createdTimestamp}`} ms\` to respond`, true);
-    embed.setFooter(`${functions.branch} by JPlexer ${functions.botver}`);
+    embed.setFooter(`${config.branch} by JPlexer ${config.botver}`);
     message.channel.send("", {
         embed
     });
