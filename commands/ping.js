c0035a622fdaa1d1d07dba57215d18f5d3609ce7
JPlexer <32966779+JPlexer@users.noreply.github.com>
2018-11-25 19:16:04 +0100

    Add files via upload

--- commands/ping.js
+++ commands/ping.js
@@ -1,4 +1,4 @@
-exports.run = (message) => {
+exports.run = (client, message, args) => {
     message.channel.send(`Pong! It took me \${${Date.now() - message.createdTimestamp}} ms\ to respond`);
     return true;
 }
