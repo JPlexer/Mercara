fabe38300a20c9859b29e4df5b57b3e919219de1
JPlexer <32966779+JPlexer@users.noreply.github.com>
2018-11-25 19:19:30 +0100

    Add files via upload

--- commands/ping.js
+++ commands/ping.js
@@ -1,4 +1,12 @@
-exports.run = (client, message, args) => {
-    message.channel.send(`Pong! It took me \${${Date.now() - message.createdTimestamp}} ms\ to respond`);
-    return true;
+exports.run = (client, message, args, Discord) => {
+    //message.channel.send(`Pong! It took me \${${Date.now() - message.createdTimestamp}} ms\ to respond`);
+    embed = new Discord.RichEmbed();
+        embed.setColor("#00FFFB");
+        embed.setAuthor(`${functions.branch} Ping`);
+        embed.addField("Pong!", `It took me \`${`${Date.now() - message.createdTimestamp}`} ms\` to respond`, true);
+        embed.setFooter(`${functions.branch} by JPlexer ${functions.botver}`);
+        message.channel.send("", {
+            embed
+        });
+        return true;
 }
