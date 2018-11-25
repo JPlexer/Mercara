b6b17733a6a77812811741426215ae63e67e3a4d
JPlexer <32966779+JPlexer@users.noreply.github.com>
2018-11-25 18:58:10 +0100

    Add files via upload

--- bot.js
+++ bot.js
@@ -1,7 +1,6 @@
 const Discord = require("discord.js");
 const Enmap = require("enmap");
 const fs = require("fs");
-​
 const client = new Discord.Client();
 const config = require("./config.js");
 client.config = config;
