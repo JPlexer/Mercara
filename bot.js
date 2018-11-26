ccccb2018db5104caae600c02280733fd9cd6aa8
JPlexer <32966779+JPlexer@users.noreply.github.com>
2018-11-26 17:08:16 +0100

    Add files via upload

--- bot.js
+++ bot.js
@@ -1,35 +1,46 @@
 const Discord = require("discord.js");
+const { promisify } = require("util");
+const readdir = promisify(require("fs").readdir);
 const Enmap = require("enmap");
-const fs = require("fs");
 
 const client = new Discord.Client();
-const config = require("./config.js");
-client.config = config;
 
-fs.readdir("./events/", (err, files) => {
-  if (err) return console.error(err);
-  files.forEach(file => {
-    const event = require(`./events/${file}`);
-    let eventName = file.split(".")[0];
-    client.on(eventName, event.bind(null, client));
-  });
-});
+client.config = require("./config.js");
 
-client.on("error", (e) => console.error(e));
-client.on("warn", (e) => console.warn(e));
-client.on("debug", (e) => console.info(e));
+client.logger = require("./modules/Logger");
+
+require("./modules/functions.js")(client);
 
 client.commands = new Enmap();
+client.aliases = new Enmap();
 
-fs.readdir("./commands/", (err, files) => {
-  if (err) return console.error(err);
-  files.forEach(file => {
-    if (!file.endsWith(".js")) return;
-    let props = require(`./commands/${file}`);
-    let commandName = file.split(".")[0];
-    console.log(`Attempting to load command ${commandName}`);
-    client.commands.set(commandName, props);
+client.settings = new Enmap({name: "settings"});
+
+const init = async () => {
+  const cmdFiles = await readdir("./commands/");
+  client.logger.log(`Loading a total of ${cmdFiles.length} commands.`);
+  cmdFiles.forEach(f => {
+    if (!f.endsWith(".js")) return;
+    const response = client.loadCommand(f);
+    if (response) console.log(response);
   });
+
+  const evtFiles = await readdir("./events/");
+  client.logger.log(`Loading a total of ${evtFiles.length} events.`);
+  evtFiles.forEach(file => {
+    const eventName = file.split(".")[0];
+    client.logger.log(`Loading Event: ${eventName}`);
+    const event = require(`./events/${file}`);
+    client.on(eventName, event.bind(null, client));
 });
 
-client.login(process.env.BOT_TOKEN);
+ client.levelCache = {};
+ for (let i = 0; i < client.config.permLevels.length; i++) {
+   const thisLevel = client.config.permLevels[i];
+   client.levelCache[thisLevel.name] = thisLevel.level;
+ }
+
+ client.login(client.config.token);
+};
+
+init();
