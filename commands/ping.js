a986c3665064c29418465fa7490f4302d5007f20
JPlexer <32966779+JPlexer@users.noreply.github.com>
2018-11-26 16:30:39 +0100

    Add files via upload

--- commands/ping.js
+++ commands/ping.js
@@ -1,5 +1,4 @@
 const Discord = require("discord.js");
-const config = require("../config.js");
 exports.run = (client, message, args) => {
     //message.channel.send(`Pong! It took me \${${Date.now() - message.createdTimestamp}} ms\ to respond`);
     embed = new Discord.RichEmbed();
