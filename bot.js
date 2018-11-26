f922008b9a3bb22d9ebfa9097f9ace82801301b5
JPlexer <32966779+JPlexer@users.noreply.github.com>
2018-11-26 16:52:49 +0100

    Add files via upload

--- bot.js
+++ bot.js
@@ -15,7 +15,9 @@ fs.readdir("./events/", (err, files) => {
   });
 });
 
-client.on("ready", () => console.log);
+client.on("error", (e) => console.error(e));
+client.on("warn", (e) => console.warn(e));
+client.on("debug", (e) => console.info(e));
 
 client.commands = new Enmap();
 
