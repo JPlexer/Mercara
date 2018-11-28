cfacf11610f917443bef064555cb349ae22a8bd1
JPlexer <32966779+JPlexer@users.noreply.github.com>
2018-11-28 12:04:56 +0100

    Add files via upload

--- commands/help.js
+++ commands/help.js
@@ -19,7 +19,7 @@ exports.run = (client, message, args, level) => {
       let command = args[0];
       if (client.commands.has(command)) {
         command = client.commands.get(command);
-        message.channel.send(`= ${command.help.name} = \n${command.help.description}\nusage: ${command.help.usage}\n= ${command.help.name} =`, {code:"asciidoc"});
+        message.channel.send(`= ${command.help.name} = \n${command.help.description}\nusage :: ${command.help.usage}\n= ${command.help.name} =`, {code:"asciidoc"});
       }
     }
   };
