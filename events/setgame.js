5d7c43bce96b9797f46ba435ad3538109eec1165
JPlexer <32966779+JPlexer@users.noreply.github.com>
2018-11-28 08:55:19 +0100

    Delete setgame.js

--- events/setgame.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    setGame: function (client) {
-      client.user.setActivity(
-        `${module.exports.prefix}help| ${module.exports.botver}`, {
-        type: "LISTENING"
-      });
-    }
-}
