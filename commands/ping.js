38f010bc75cd84875bb471f9409ee0e30ad7343a
JPlexer <32966779+JPlexer@users.noreply.github.com>
2018-11-28 08:54:20 +0100

    Add files via upload

--- commands/ping.js
+++ commands/ping.js
@@ -1,12 +1,23 @@
-exports.run = (Discord, message) => {
-    //message.channel.send(`Pong! It took me \${${Date.now() - message.createdTimestamp}} ms\ to respond`);
-    embed = new Discord.RichEmbed();
-        embed.setColor("#00FFFB");
-        embed.setAuthor(`${config.branch} Ping`);
-        embed.addField("Pong!", `It took me \`${`${Date.now() - message.createdTimestamp}`} ms\` to respond`, true);
-        embed.setFooter(`${config.branch} by JPlexer ${config.botver}`);
-        message.channel.send("", {
-            embed
-        });
-        return true;
-}
+//exports.run = (Discord, message) => {
+    ////message.channel.send(`Pong! It took me \${${Date.now() - message.createdTimestamp}} ms\ to respond`);
+    //embed = new Discord.RichEmbed();
+        //embed.setColor("#00FFFB");
+        //embed.setAuthor(`${client.config.branch} Ping`);
+        //embed.addField("Pong!", `It took me \`${`${Date.now() - message.createdTimestamp}`} ms\` to respond`, true);
+        //embed.setFooter(`${client.config.branch} by JPlexer ${client.config.botver}`);
+        //message.channel.send("", {
+        //    embed
+        //});
+        //return true;
+//}
+exports.run = async (client, message, args, level) => {
+    const msg = await message.channel.send("Ping?");
+    msg.edit(`Pong! It took me \${${Date.now() - message.createdTimestamp}} ms\ to respond`);
+  };
+  
+  exports.help = {
+    name: "ping",
+    category: "Miscelaneous",
+    description: "It like... Pings. Then Pongs. And it's not Ping Pong.",
+    usage: "ping"
+  };
