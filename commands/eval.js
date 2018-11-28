0c3829ceefce41069638c9733bcdb5f5e329d654
JPlexer <32966779+JPlexer@users.noreply.github.com>
2018-11-28 11:22:36 +0100

    Add files via upload

---
+++ commands/eval.js
@@ -0,0 +1,34 @@
+const Discord = require("discord.js");
+exports.run = async (client, message, args) => {
+    if (message.author.id != client.config.botowner) return message.channel.send("**Hey " + message.author.username + ", only JPlexer can use that!**");
+    const code = args.join(" ");
+    try {
+      const evaled = eval(code);
+      const clean = await client.clean(client, evaled);
+
+      if (evaled.includes(client.token)) {
+        evaled = evaled.replace(client.token, "THATS MY PRIVESSEESEES NO! AND I MEAN NO! \n\ YOU GET I WILL CALL THE COPS 👮");
+
+        if (evaled.includes(client.token)) {
+            evaled = evaled.replace(client.token, "THATS MY PRIVESSEESEES NO! AND I MEAN NO! \n\ YOU GET I WILL CALL THE COPS 👮");
+        }
+    }
+
+    let embed = new Discord.RichEmbed()
+        .addField(`:arrow_down:Input`, "***```" + code + "```***", true)
+        .addField(`:arrow_up:Output`, `\`\`\`js\n${clean}\n\`\`\``, )
+        .setColor("#00FFFB") //
+        .setTimestamp()
+
+    message.channel.send(embed);
+    } catch (err) {
+      message.channel.send(`\`ERROR\` \`\`\`xl\n${await client.clean(client, err)}\n\`\`\``);
+    }
+  };
+  
+  exports.help = {
+    name: "eval",
+    category: "System",
+    description: "Evaluates arbitrary javascript.",
+    usage: "eval [...code]"
+  };
