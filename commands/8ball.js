92a4dcc485487c31945338d12faf220f4eb5477b
JPlexer <32966779+JPlexer@users.noreply.github.com>
2018-12-10 16:56:57 +0100

    Add files via upload

---
+++ commands/8ball.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js");
+exports.run = async (client, message, args) => {
+    var rnd = Math.random(1,5);
+    console.log(rnd);
+    if(rnd===1) message.channel.send("No.");
+    else if(rnd===2) message.channel.send("Not Probable.");
+    else if(rnd===3) message.channel.send("Maybe.");
+    else if(rnd===4) message.channel.send("Probably.");
+    else if(rnd===5) message.channel.send("Yes.");
+};
+
+exports.help = {
+    name: "8ball",
+    category: "Fun",
+    description: "Ask a Question! It will answer it.",
+    usage: "8ball [question]"
+};
