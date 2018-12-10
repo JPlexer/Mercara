08c9e38c979ac3b6cad231c9843aa2c6f26aaef2
JPlexer <32966779+JPlexer@users.noreply.github.com>
2018-12-10 18:38:08 +0100

    Add files via upload

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,10 +1,16 @@
 exports.run = (client, message, args) => {
-    let avatar = message.mentions.users.size ? message.mentions.users.avatarURL() : message.author.avatarURL();
+    /*let avatar = message.mentions.users.size ? message.mentions.users.avatarURL() : message.author.avatarURL();
     if (message.mentions.users.size > 0) {
         message.channel.send(`Avatar for, **${message.mentions.users.username}:**\n${avatar}`);
     } else {
       message.channel.send(`Avatar for, **${message.author.username}:**\n${avatar}`);
-    }
+    }*/
+
+    if (message.mentions.users.size == 0) {
+        message.reply("No results! You must ping someone (TIP: If you want your Pic, try pinging yourself)");
+    } else {
+        message.reply(users[0].displayAvatarURL);
+}
 }
   
   exports.help = {
