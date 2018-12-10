691a5510cc8bbef71ce5ac1380c312a699a4df0e
JPlexer <32966779+JPlexer@users.noreply.github.com>
2018-12-10 18:27:14 +0100

    Add files via upload

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,7 +1,7 @@
 exports.run = (client, msg, args) => {
-    let avatar = msg.mentions.users.size ? msg.mentions.users.first().avatarURL() : msg.author.avatarURL();
+    let avatar = msg.mentions.users.size ? msg.mentions.users.avatarURL() : msg.author.avatarURL();
     if (msg.mentions.users.size > 0) {
-        msg.channel.send(`Avatar for, **${msg.mentions.users.first().username}:**\n${avatar}`);
+        msg.channel.send(`Avatar for, **${msg.mentions.users.username}:**\n${avatar}`);
     } else {
       msg.channel.send(`Avatar for, **${msg.author.username}:**\n${avatar}`);
     }
